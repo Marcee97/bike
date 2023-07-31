1cd4bcb305690b3da4008a8b16a144304d25233a
Marcelo Quadrilatero <marceloquadrilatero@gmail.com>
2023-07-31 15:50:04 -0300

    versionado

--- javascript/archivo.js
+++ javascript/archivo.js
@@ -1,15 +1,54 @@
 
-const menu = document.querySelector('.fa-solid');
-const contenidoDesplegable = document.querySelector('.contenido-desplegable');
+const menu                 = document.querySelector('.btn-menu');
 const botonCerrar          = document.querySelector('.btn-cerrar');
+const contenedorImagenes   = document.querySelector('.grande');
+const menuDesplegable      = document.querySelector('.nav');
+const titulo               = document.querySelector('.titulo');
 
 
 menu.addEventListener('click',()=>{
-    contenidoDesplegable.classList.toggle('activo')
+    menuDesplegable.classList.toggle('activo');
+    titulo.classList.toggle('none')
+
 })
 
-botonCerrar.addEventListener('click',()=>{
-    contenidoDesplegable.classList.remove('activo')
-})
+
+    
+    
+
+
+// let index = 1;
+// let cantidadDeImagenes = document.querySelectorAll('.img');
+
+// setInterval(function () {
+//     let porcentaje = index * -33.3;
+//     contenedorImagenes.style.transform = "translateX("+ porcentaje +"%)";
+//     index++;
+
+//     if(cantidadDeImagenes.length == index){
+//         index = 0
+//     }
+// },3000)
+
+
+
+
+
+
+// function desplazar() {
+//   contenedorImagenes.style.transform = 'translateX(-33.3%)';
+  
+//   cantidadRepeticiones--;
+  
+//   if (cantidadRepeticiones > 0) {
+//     // Si quedan repeticiones, llamar a setTimeout para repetir el desplazamiento
+//     setTimeout(desplazar, 3000);
+//   }
+// }
+
+// // Iniciar el desplazamiento
+// desplazar();
+
+
 
 
